92801ba8f8dfea6b921c522e4bf0a6fa2d114232
Mohammad Ataei <mammadataei@gmail.com>
2020-06-10 15:55:24 +0430

    Extract Overview section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -213,103 +213,7 @@
       </div>
     </section>
 
-    <section class="relative block bg-gray-800">
-      <div class="container px-4 py-32 mx-auto ">
-        <div class="flex flex-wrap justify-center text-center">
-          <div class="w-full px-4 lg:w-6/12">
-            <h2 class="text-4xl font-semibold text-white">Overview</h2>
-
-            <p class="mt-4 mb-4 text-lg leading-relaxed text-gray-500">
-              HyperDbg is extended based on the following assumptions.
-            </p>
-          </div>
-        </div>
-
-        <div class="flex flex-wrap justify-center mt-32">
-          <div class="w-full px-4 mb-12 text-left lg:w-4/12">
-            <img
-              svg-inline
-              class="inline-block w-12 h-12 text-gray-600 fill-current focus:outline-none"
-              src="@/assets/svg/feature_3.svg"
-            />
-
-            <h6 class="mt-5 text-xl font-semibold text-white">Active Development</h6>
-            <p class="mt-2 mb-4 text-gray-500">
-              HyperDbg is globaly under active development, many people contribute in its design.
-            </p>
-          </div>
-
-          <div class="w-full px-4 mb-12 text-left lg:w-4/12">
-            <img
-              svg-inline
-              class="inline-block w-12 h-12 text-gray-600 fill-current focus:outline-none"
-              src="@/assets/svg/feature_1.svg"
-            />
-
-            <h5 class="mt-5 text-xl font-semibold text-white">
-              Rely on Academic Papers
-            </h5>
-
-            <p class="mt-2 mb-4 text-gray-500">
-              We extend HyperDbg's new features based on academic papers.
-            </p>
-          </div>
-
-          <div class="w-full px-4 mb-12 text-left lg:w-4/12">
-            <img
-              svg-inline
-              class="inline-block w-12 h-12 text-gray-600 fill-current focus:outline-none"
-              src="@/assets/svg/feature_2.svg"
-            />
-
-            <h5 class="mt-5 text-xl font-semibold text-white">Custom Scripts</h5>
-            <p class="mt-2 mb-4 text-gray-500">
-              You can create custom Python scripts and use all HyperDbg features automatically in a way you want.
-            </p>
-          </div>
-
-          <div class="w-full px-4 mb-12 text-left lg:w-4/12">
-            <img
-              svg-inline
-              class="inline-block w-12 h-12 text-gray-600 fill-current focus:outline-none"
-              src="@/assets/svg/feature_6.svg"
-            />
-
-            <h5 class="mt-5 text-xl font-semibold text-white">Integration with Static Analysis Tools</h5>
-            <p class="mt-2 mb-4 text-gray-500">
-              One of the goals of HyperDbg is to make an easy integration with static analysis tools like IDA, Ghidra and other tools.
-            </p>
-          </div>
-
-          <div class="w-full px-4 mb-12 text-left lg:w-4/12">
-            <img
-              svg-inline
-              class="inline-block w-12 h-12 text-gray-600 fill-current focus:outline-none"
-              src="@/assets/svg/feature_5.svg"
-            />
-
-            <h5 class="mt-5 text-xl font-semibold text-white">Heavily Hardware-dependant</h5>
-            <p class="mt-2 mb-4 text-gray-500">
-              HyperDbg extensively uses hardware technologies to ease the reverse-engineering.
-            </p>
-          </div>
-
-          <div class="w-full px-4 mb-12 text-left lg:w-4/12">
-            <img
-              svg-inline
-              class="inline-block w-12 h-12 text-gray-600 fill-current focus:outline-none"
-              src="@/assets/svg/feature_4.svg"
-            />
-
-            <h5 class="mt-5 text-xl font-semibold text-white">Have a New Idea?</h5>
-            <p class="mt-2 mb-4 text-gray-500">
-              Open an issue and describe how to implement it or you can submit a pull request with you feature.
-            </p>
-          </div>
-        </div>
-
-      </div>
-    </section>
+    <section-overview />
 
     <section-faq />
   </div>
@@ -317,12 +221,14 @@
 
 <script>
 import SectionFaq from '@/components/home/FAQ.vue'
+import SectionOverview from '@/components/home/Overview.vue'
 
 export default {
   name: 'Home',
 
   components: {
-    SectionFaq
+    SectionFaq,
+    SectionOverview
   }
 }
 </script>
