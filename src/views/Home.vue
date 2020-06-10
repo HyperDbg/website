df5ca3b5e40977b3a59b5953e824deb89391f4c5
Mohammad Ataei <mammadataei@gmail.com>
2020-06-10 15:51:30 +0430

    Extract Faq section to dedicated component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -311,40 +311,18 @@
       </div>
     </section>
 
-    <section class="relative py-20">
-
-      <div class="container px-4 mx-auto lg:my-64">
-        <div class="flex flex-wrap items-center">
-          <div class="right-0 top-auto w-full lg:absolute md:w-8/12 lg:w-6/12 xl:w-6/12">
-            <img
-              svg-inline
-              class="block w-auto h-auto text-gray-900 fill-current focus:outline-none"
-              src="@/assets/svg/question.svg"
-            />
-          </div>
-
-          <div class="w-full px-4 mr-auto md:w-6/12">
-            <div class="md:pl-12">
-
-              <h3 class="text-3xl font-semibold text-gray-800">
-                FAQs
-              </h3>
-
-              <p class="mt-3 text-lg leading-relaxed text-gray-600">
-                Here's a fast guide of how to compile and run HyperDbg on your system, also there are some interesting examples of how you can use HyperDbg in your reverse-engineering journey.
-              </p>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <section-faq />
   </div>
 </template>
 
 <script>
+import SectionFaq from '@/components/home/FAQ.vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+
+  components: {
+    SectionFaq
+  }
 }
 </script>
