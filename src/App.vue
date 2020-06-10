de6b774255ab909f5644bca2dbb152a69840580c
Mohammad Ataei <mammadataei@gmail.com>
2020-06-10 15:45:55 +0430

    Extract footer to dedicated component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <app-header></app-header>
     <router-view />
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/Header'
+import AppFooter from '@/components/Footer'
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 }
 </script>
